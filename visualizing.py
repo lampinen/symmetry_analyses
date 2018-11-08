8e7d499dc1b2f19afbad1c01b45b11ddbdde5872
Andrew Lampinen <lampinen@stanford.edu>
2018-11-08 14:23:19 -0800

    visualizing the datasets

---
+++ visualizing.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+x_data = np.eye(4)
+y_data = np.array(
+    [[1, 1, 0, 0.5, 0, 0.5, 0],
+     [1, 1, 0, 0, 0.5, 0, 0.5],
+     [1, 0, 1, 0.5, 0, 0.5, 0],
+     [1, 0, 1, 0, 0.5, 0, 0.5]])
+U, S, V, = np.linalg.svd(y_data)
+y_data_asymm = np.zeros_like(y_data)
+y_data_asymm[range(len(S)), range(len(S))] = S
+
+plot.figure()
+plot.imshow(y_data)
+plot.colorbar()
+plot.savefig('symmetric_data.png')
+
+plot.figure()
+plot.imshow(y_data_asymm)
+plot.colorbar()
+plot.savefig('asymmetric_data.png')
