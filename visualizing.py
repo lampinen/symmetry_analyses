5a7436b0669424ca30652e0b64cf5b3baf455a80
Andrew Lampinen <lampinen@stanford.edu>
2018-11-14 12:32:56 -0800

    difference appears to be due to number of outputs used, not symmetry per se

--- visualizing.py
+++ visualizing.py
@@ -1,14 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-x_data = np.eye(4)
-y_data = np.array(
-    [[1, 1, 0, 0.5, 0, 0.5, 0],
-     [1, 1, 0, 0, 0.5, 0, 0.5],
-     [1, 0, 1, 0.5, 0, 0.5, 0],
-     [1, 0, 1, 0, 0.5, 0, 0.5]])
-U, S, V, = np.linalg.svd(y_data)
+y_data = np.loadtxt("symmetric_data.csv", delimiter=",") 
 y_data_asymm = np.loadtxt("asymmetric_data.csv", delimiter=",") 
+y_data_asymm2 = np.loadtxt("asymmetric_data_2.csv", delimiter=",") 
 
 plot.figure()
 plot.imshow(y_data)
@@ -19,3 +14,8 @@ plot.figure()
 plot.imshow(y_data_asymm)
 plot.colorbar()
 plot.savefig('plots/asymmetric_data.png')
+
+plot.figure()
+plot.imshow(y_data_asymm2)
+plot.colorbar()
+plot.savefig('plots/asymmetric_data_2.png')
